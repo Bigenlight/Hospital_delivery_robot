ab7229336ce3cfe5654da547fd45dfb889a659ce
imjunhyung <lyhid2@naver.com>
2023-09-20 03:27:20 +0900

    new commit

---
+++ LJH_ws/turtlebot3_ws (copy)/build/getpo/CMakeFiles/getpo.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/getpo.dir/src/getpo.cpp.o"
+  "CMakeFiles/getpo.dir/src/getpo.cpp.o.d"
+  "getpo"
+  "getpo.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/getpo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
